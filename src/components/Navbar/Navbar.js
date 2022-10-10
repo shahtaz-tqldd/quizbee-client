48a92e1d6f2a01181c11989be332266675fa1699
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-10 21:24:16 +0600

    navbar added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,18 +4,22 @@ import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div className='nav-logo'>
-            <img src={logo} alt="quiz bee"></img>
-        </div>
-        <div className='nav-items'>
-            <Link to='/topics'>Topics</Link>
-            <Link to='/statistics'>Statistics</Link>
-            <Link to='/blogs'>Blogs</Link>
-        </div>
-    </nav>
-  )
+    return (
+        <nav>
+            <div className='nav-container'>
+
+                <div className='nav-logo'>
+                    <img src={logo} alt="quiz bee"></img>
+                    <span>quiz bee</span>
+                </div>
+                <div className='nav-items'>
+                    <Link to='/topics'>Topics</Link>
+                    <Link to='/statistics'>Statistics</Link>
+                    <Link to='/blogs'>Blogs</Link>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
