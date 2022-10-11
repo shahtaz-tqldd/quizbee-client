3b7d3fee35158f02fb8ff0a9f22edeea920a661d
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-11 22:46:52 +0600

    statistics page graph added

--- src/components/QuestionCard/QuestionCard.js
+++ src/components/QuestionCard/QuestionCard.js
@@ -1,25 +1,44 @@
 import React, { useState } from 'react'
 import './QuestionCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const QuestionCard = ({ ques, index }) => {
     const [shown, setShown] = useState(false)
 
+    // const [color, setColor] = useState()
+
     const { question, correctAnswer, id, options } = ques
+
+    const handleClickedAnswer = (id, index) => {
+        const right = options[index]
+        if (right === correctAnswer) {
+            console.log('right')
+            alert('Right Answer')
+        } else {
+            console.log('wrong')
+            alert('Wrong Answer')
+        }
+        // console.log(id, index, right)
+        // setColor(!color)
+    }
     return (
         <div className='question-container'>
             <div className='question-head'>
-                <span>Question No. {index +1}</span>
-                <FontAwesomeIcon onClick={()=> setShown(!shown)} className='see-answer' icon ={faEye} />
+                <span>Question No. {index + 1}</span>
+                <FontAwesomeIcon onClick={() => setShown(!shown)} className='see-answer' icon={faEye} />
             </div>
 
             <div className='question-title' dangerouslySetInnerHTML={{ __html: question }}></div>
-            
+
             <div className='question-options'>
                 {options.map((op, index) => {
                     let ans;
-                    switch(index){
+                    switch (index) {
                         case 0:
                             ans = 'a';
                             break;
@@ -35,15 +54,19 @@ const QuestionCard = ({ ques, index }) => {
                         default:
                             ans = '';
                     }
-                    return(
-                    <div className='option'>
-                        {`${ans}. `}
-                        {op}
-                    </div>
-                    )})}
+                    return (
+                        // <div onClick={()=> handleClickedAnswer(id, index)} className={`option ${color? 'clicked' : 'unclicked'}`}>
+                        <div onClick={() => handleClickedAnswer(id, index)} className={`option`}>
+                            {`${ans}. `}{op}
+                            
+                        </div>
+                        
+                    )
+                })}
             </div>
+            <ToastContainer />
             <div className={`correct-ans ${shown ? 'shown' : 'hide'}`}>
-                <b>Correct Answer:</b> {correctAnswer}
+                <b>Correct Answer : &nbsp;</b> {correctAnswer}
             </div>
         </div>
     )
--- src/pages/StatPage/StatPage.js
+++ src/pages/StatPage/StatPage.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
 import './StatPage.css'
 
 const StatPage = () => {
+  const ques = useLoaderData()
+  const dat = ques.data
+  const data = [
+    { name: `${dat[0].name}`, Topic1: `${dat[0].total}`, amt: 20 },
+    { name: `${dat[1].name}`, Topic2: `${dat[1].total}`, amt: 20 },
+    { name: `${dat[2].name}`, Topic3: `${dat[2].total}`, amt: 20 },
+    { name: `${dat[3].name}`, Topic4: `${dat[3].total}`, amt: 20 },
+  ];
+
   return (
-    <div>StatPage</div>
+    <div className='graph'>
+      <h2>Graph of Total Question</h2>
+      <div className='chart'>
+
+        <BarChart width={730} height={350} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Topic1" fill="#82ca9d" />
+          <Bar dataKey="Topic2" fill="#fffa9e" />
+          <Bar dataKey="Topic3" fill="#eeca9c" />
+          <Bar dataKey="Topic4" fill="#22c34e" />
+        </BarChart>
+      </div>
+    </div>
   )
 }
 
