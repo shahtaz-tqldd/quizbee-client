0e050681fa2ef86f851fb35a0f052e752289179d
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-11 15:56:12 +0600

    correct answer shown on click

--- src/components/QuestionCard/QuestionCard.js
+++ src/components/QuestionCard/QuestionCard.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './QuestionCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import { faEye } from '@fortawesome/free-solid-svg-icons'
 
 const QuestionCard = ({ ques, index }) => {
+    const [shown, setShown] = useState(false)
+
     const { question, correctAnswer, id, options } = ques
     return (
         <div className='question-container'>
             <div className='question-head'>
                 <span>Question No. {index +1}</span>
-                <FontAwesomeIcon className='see-answer' icon ={faEye} />
+                <FontAwesomeIcon onClick={()=> setShown(!shown)} className='see-answer' icon ={faEye} />
             </div>
+
             <div className='question-title' dangerouslySetInnerHTML={{ __html: question }}></div>
             
             <div className='question-options'>
@@ -38,6 +41,9 @@ const QuestionCard = ({ ques, index }) => {
                         {op}
                     </div>
                     )})}
+            </div>
+            <div className={`correct-ans ${shown ? 'shown' : 'hide'}`}>
+                <b>Correct Answer:</b> {correctAnswer}
             </div>
         </div>
     )
