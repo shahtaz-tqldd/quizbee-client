b4a426b3fbbdc51ee2ec1bd3ce646ddf7ece960e
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-10-12 20:16:16 +0600

    navbar responsive done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
-      Footer
+      All Rights Reserved to &copy; Quiz Bee
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faSignIn, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     return (
         <nav>
             <div className='nav-container'>
+
+                <div className='toggle-icon' onClick={() => setOpen(!open)}>
+                    {open ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />}
+                </div>
+
                 <div className='nav-logo'>
                     <Link to='/'>
                         <img src={logo} alt="quiz bee"></img>
                         <span>quiz bee</span>
                     </Link>
                 </div>
-                <div className='nav-items'>
+
+                <div className={`nav-items ${open ? 'shown' : 'hide'}`}>
                     <Link to='/topics'>Topics</Link>
                     <Link to='/statistics'>Statistics</Link>
                     <Link to='/blogs'>Blogs</Link>
+                </div>
+
+                <div className='toggle-icon'>
+                    <FontAwesomeIcon icon={faSignIn} />
                 </div>
             </div>
         </nav>
